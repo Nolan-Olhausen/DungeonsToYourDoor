578ea0c58883641929dbaf428868c0cdac6d41ee
Nolan-Olhausen <olhausennolan@gmail.com>
2025-05-27 23:28:37 -0600

    Fixed CSS for home and text responsiveness

--- src/components/AdventuresSection.js
+++ src/components/AdventuresSection.js
@@ -5,6 +5,10 @@ import "../index.css";
 function AdventuresSection() {
   return (
     <div className="adventures-section" id="Adventures">
+      <h1>The Adventures</h1>
+      <h1>In Action</h1>
+      <p>Where heroes rose to the challenge, epic battles were fought, and unforgettable stories were made. Browse through our gallery to get a glimpse of the action and excitement that await in your next adventure!</p>
+
       
     </div>
   );
