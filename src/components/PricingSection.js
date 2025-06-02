62030b9c169b1087785601b56fed438c909a24b8
Nolan-Olhausen <olhausennolan@gmail.com>
2025-06-01 22:09:28 -0600

    progress on pricing desktp layout

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import "../index.css";
 import d20Icon from "../assets/d20FillLogo.png";
 import campaignImage from "../assets/inn.jpg";
+import overlayImage from "../assets/campfire.png";
 
 const campaigns = [
   {
@@ -44,50 +45,74 @@ function PricingSection() {
 
   return (
     <div className="pricing-section" id="Pricing">
-      <h1>The Price of</h1>
-      <h1>Adventure</h1>
-      <p>
-        Every quest has its cost, but glory, laughter, and unforgettable
-        memories are always included. Choose from a selection of ready-to-run
-        one-shot adventures, each priced for epic fun and hassle-free game
-        nights. Your next great story is just a roll away.
-      </p>
+      <div className="mobile-pricing-section">
+        <h1>The Price of</h1>
+        <h1>Adventure</h1>
+        <p>
+          Every quest has its cost, but glory, laughter, and unforgettable
+          memories are always included. Choose from a selection of ready-to-run
+          one-shot adventures, each priced for epic fun and hassle-free game
+          nights. Your next great story is just a roll away.
+        </p>
 
-      <div className="pricing-slideshow-container">
-        {campaigns.map((camp, index) => (
-          <div
-            key={index}
-            className={`pricing-card ${index === currentIndex ? "active" : ""}`}
-            style={{
-              "--bg-image": `url(${camp.backgroundImage})`,
-            }}
-          >
-            <div className="card-top">
-              <h2>{camp.title}</h2>
-              <p className="campaign-players">{camp.players}</p>
-            </div>
+        <div className="pricing-slideshow-container">
+          {campaigns.map((camp, index) => (
+            <div
+              key={index}
+              className={`pricing-card ${
+                index === currentIndex ? "active" : ""
+              }`}
+              style={{
+                "--bg-image": `url(${camp.backgroundImage})`,
+              }}
+            >
+              <div className="card-top">
+                <h2>{camp.title}</h2>
+                <p className="campaign-players">{camp.players}</p>
+              </div>
 
-            <div className="card-description">
-              <p className="campaign-description">{camp.description}</p>
-            </div>
+              <div className="card-description">
+                <p className="campaign-description">{camp.description}</p>
+              </div>
 
-            <div className="card-bottom">
-              <h3>{camp.deposit}</h3>
-              <h3>{camp.price}</h3>
+              <div className="card-bottom">
+                <h3>{camp.deposit}</h3>
+                <h3>{camp.price}</h3>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="pricing-nav">
+          {campaigns.map((_, index) => (
+            <img
+              key={index}
+              src={d20Icon}
+              alt={`Go to card ${index}`}
+              className={`nav-icon ${index === currentIndex ? "active" : ""}`}
+              onClick={() => setCurrentIndex(index)}
+            />
+          ))}
+        </div>
       </div>
-      <div className="pricing-nav">
-        {campaigns.map((_, index) => (
+      <div className="desktop-pricing-section">
+        <div className="overlay-text">
+          <h1>The Price of</h1>
+          <h1>Adventure</h1>
+          <p>
+            Every quest has its cost, but glory, laughter, and unforgettable
+            memories are always included. Choose from a selection of
+            ready-to-run one-shot adventures, each priced for epic fun and
+            hassle-free game nights. Your next great story is just a roll away.
+          </p>
+        </div>
+
+        <div className="dm-image-container">
           <img
-            key={index}
-            src={d20Icon}
-            alt={`Go to card ${index}`}
-            className={`nav-icon ${index === currentIndex ? "active" : ""}`}
-            onClick={() => setCurrentIndex(index)}
+            src={overlayImage}
+            alt="Adventure Awaits"
+            className="overlay-background"
           />
-        ))}
+        </div>
       </div>
     </div>
   );
