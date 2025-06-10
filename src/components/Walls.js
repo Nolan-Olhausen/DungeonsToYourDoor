0efcc742f552e6df12b8db60f7a18b18d374a570
Nolan-Olhausen <olhausennolan@gmail.com>
2025-06-10 03:23:28 -0600

    adjust dice left wall to avoid slant in desktop

--- src/components/Walls.js
+++ src/components/Walls.js
@@ -22,14 +22,14 @@ export default function Walls() {
   // Orthographic camera bounds at z=0
   const width = size.width / zoom / 2;
   const height = size.height / zoom / 2;
-  const wallOffset = 3; // Buffer to avoid clipping at edges
+  const wallOffset = 3.75; // Buffer to avoid clipping at edges
 
   return (
     <>
       {/* Left wall */}
       <Wall
         rotation={[0, Math.PI / 2, 0]}
-        position={[-width / 2 - wallOffset, 0, 0]}
+        position={[-width / 2 + 1, 0, 0]}
       />
       {/* Right wall */}
       <Wall
