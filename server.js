f08019a6602da270d8078ffa5a5121579a0ee58f
Nolan Olhausen <olhausennolan@gmail.com>
2025-07-16 17:02:48 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
